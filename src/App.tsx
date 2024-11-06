412da6ced137f37d403f93cdf1141ffc478585d5
Serhii Honcharov <sergeygoncharov9@gmail.com>
2024-11-06 15:41:13 +0200

    (common pages and routing) mock common pages were added. Reusable component window was created too, Routing is provided.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import AboutPage from './Pages/AboutPage';
+import UserRegistrationPage from './Pages/UserRegistrationPage';
+import UserAuthorizationPage from './Pages/UserAuthorizationPage';
+import UsersListPage from './Pages/UsersListPage';
+import CompaniesListPage from './Pages/CompaniesListPage';
+import CompanyProfilePage from './Pages/CompanyProfilePage';
+import UserProfilerPage from "./Pages/UserProfilerPage";
+import ModalWindow from "./Compponents/ModalWindow";
 
-const App: React.FC = () => {
-  const projectName = "My Awesome Project";
-  const greeting = "Welcome to my project!";
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>{projectName}</h1>
-        <p>{greeting}</p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h1>React App with Mock Data</h1>
+        <Routes>
+          <Route path="/" element={<AboutPage />} />
+          <Route path="/register" element={<UserRegistrationPage />} />
+          <Route path="/login" element={<UserAuthorizationPage />} />
+          <Route path="/users" element={<UsersListPage />} />
+          <Route path="/users/:id" element={<UserProfilerPage />} />
+          <Route path="/companies" element={<CompaniesListPage />} />
+          <Route path="/companies/:id" element={<CompanyProfilePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
