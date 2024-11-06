fd319b3c0c1d694d4536f21d19f64fbc62d4a7fd
Serhii Honcharov <sergeygoncharov9@gmail.com>
2024-11-06 12:07:00 +0200

    sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const projectName = "My Awesome Project";
+  const greeting = "Welcome to my project!";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{projectName}</h1>
+        <p>{greeting}</p>
       </header>
     </div>
   );
